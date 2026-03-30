e02c898f3f7b9d6ce31e8f0d7a7824994c8fc62c
komed3 <pk97@outlook.de>
2026-03-30 20:28:13 +0200

    create app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-import { type JSX } from 'react';
+import { useEffect, type JSX } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
-export default function App () : JSX.Element {}
+export default function App () : JSX.Element {
+    return ( <Router>
+        <div className="min-h-screen flex flex-col relative overflow-hidden selection:bg-brutal-pink selection:text-brutal-white">
+            <main className="flex-grow relative z-10">
+                <Routes>
+                    <Route path="/" element={ <LandingPage /> } />
+                    <Route path="/project/:repoName" element={ <ProjectPage /> } />
+                </Routes>
+            </main>
+        </div>
+    </Router> );
+}
