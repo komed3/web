5311482ba693053ba0b7361d65e935a98f9e8159
komed3 <pk97@outlook.de>
2026-03-31 01:40:44 +0200

    prep skill-set section

--- src/components/Skillset.tsx
+++ src/components/Skillset.tsx
@@ -1,3 +1,9 @@
+import { SectionHeading } from './SectionHeading';
+
+
 export function Skillset () {
-    return ( <></> );
+    return ( <> <section className="space-y-6 md:space-y-8">
+        <SectionHeading title="Skillset_" />
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-3 md:gap-4"></div>
+    </section> </> );
 }
