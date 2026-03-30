2df0cbe4574a8453484d3b32792eac49b772d2ab
komed3 <pk97@outlook.de>
2026-03-31 01:26:34 +0200

    define ASCII logo

---
+++ src/components/AsciiLogo.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+
+const CHARS = '!@#$%^&*()_+-=[]{}|;:,.<>?/0123456789ABCDEF';
+const LOGO_GRID = [
+    "            XXXX      ",
+    "          XXXXXXXX    ",
+    "        XXXXXXXXXXXX  ",
+    "          XXXXXXXX    ",
+    "            XXXX      ",
+    "    XXXX              ",
+    "  XXXXXXXX            ",
+    "XXXXXXXXXXXX          ",
+    "  XXXXXXXX            ",
+    "    XXXX              ",
+    "            XXXX      ",
+    "          XXXXXXXX    ",
+    "        XXXXXXXXXXXX  ",
+    "          XXXXXXXX    ",
+    "            XXXX      "
+];
