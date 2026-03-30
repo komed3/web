00f66b40c9aa113328bd17ba0c76ce03eadd20da
komed3 <pk97@outlook.de>
2026-03-31 00:16:17 +0200

    add cursor + bg effects

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import type { JSX } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Footer } from './components/Footer';
+import { Background } from './effects/Background';
+import { Cursor } from './effects/Cursor';
 import { ScrollToTop } from './lib/scrollToTop';
 
 
@@ -9,6 +11,8 @@ export default function App () : JSX.Element {
     return ( <Router>
         <ScrollToTop />
         <div className="min-h-screen flex flex-col relative overflow-hidden selection:bg-brutal-pink selection:text-brutal-white">
+            <Cursor />
+            <Background />
             <main className="flex-grow relative z-10">
                 <Routes>
                     <Route path="/" element={ <LandingPage /> } />
