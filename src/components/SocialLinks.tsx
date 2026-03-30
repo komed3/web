54e6d4cea7623f72f155b2629538a5300fe1c415
komed3 <pk97@outlook.de>
2026-03-31 00:34:36 +0200

    fix missing social links

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,11 +1,12 @@
-import { Github, Package, Twitter, Coffee, Archive } from 'lucide-react';
+import { Package, Coffee, Archive } from 'lucide-react';
 import { cn } from '../lib/utils';
+import { siGithub, siX } from 'simple-icons';
 
 
 export const SOCIAL_LINKS = [
-    { name: 'GitHub', icon: Github, url: 'https://github.com/komed3', color: 'bg-brutal-blue text-brutal-white' },
+    { name: 'GitHub', icon: siGithub.svg, url: 'https://github.com/komed3', color: 'bg-brutal-blue text-brutal-white' },
     { name: 'npmjs', icon: Package, url: 'https://npmjs.com/~komed3', color: 'bg-brutal-pink text-brutal-white' },
-    { name: 'X / Twitter', icon: Twitter, url: 'https://x.com/komed3dev', color: 'bg-brutal-green text-brutal-black' },
+    { name: 'X / Twitter', icon: siX.svg, url: 'https://x.com/komed3dev', color: 'bg-brutal-green text-brutal-black' },
     { name: 'Ko-fi', icon: Coffee, url: 'https://ko-fi.com/komed3', color: 'bg-brutal-orange text-brutal-black' },
     { name: 'APT Repo', icon: Archive, url: 'https://deb.komed3.de', color: 'bg-brutal-yellow text-brutal-black' }
 ];
