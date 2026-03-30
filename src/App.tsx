4a8245684e24fbb9f8454b0a9941d3c48ba23b39
komed3 <pk97@outlook.de>
2026-03-30 20:30:54 +0200

    implement scroll to top effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { useEffect, type JSX } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import type { JSX } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { ScrollToTop } from './lib/scrollToTop';
+
 
 export default function App () : JSX.Element {
     return ( <Router>
+        <ScrollToTop />
         <div className="min-h-screen flex flex-col relative overflow-hidden selection:bg-brutal-pink selection:text-brutal-white">
             <main className="flex-grow relative z-10">
                 <Routes>
---
+++ src/lib/scrollToTop.ts
@@ -0,0 +1,8 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
+export function ScrollToTop () {
+    useEffect( () => window.scrollTo( 0, 0 ), [ useLocation() ] );
+    return null;
+}
