e8860fc0ff5f6cefebdcf76c8db5e0fd36641ce3
komed3 <pk97@outlook.de>
2026-03-31 00:07:11 +0200

    create background effect

---
+++ src/effects/Background.tsx
@@ -0,0 +1,16 @@
+export function BackgroundElements () {
+    const [ , setMousePos ] = useState ( { x: 0, y: 0 } );
+    const springConfig = { damping: 25, stiffness: 400 };
+    const mouseX = useSpring( 0, springConfig );
+    const mouseY = useSpring( 0, springConfig );
+
+    useEffect( () => {
+        const handleMouseMove = ( e: MouseEvent ) => {
+            mouseX.set( e.clientX ), mouseY.set( e.clientY );
+            setMousePos( { x: e.clientX, y: e.clientY } );
+        };
+
+        window.addEventListener( 'mousemove', handleMouseMove );
+        return () => window.removeEventListener( 'mousemove', handleMouseMove );
+    }, [ mouseX, mouseY ] );
+}
