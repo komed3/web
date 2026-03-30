f94a244a711f44dba118b205c4139dae6844558c
komed3 <pk97@outlook.de>
2026-03-30 19:35:09 +0200

    define default config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'node:path';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig, loadEnv, type Plugin } from 'vite';
+import { defineConfig, type Plugin } from 'vite';
 
 
 function postBuildPlugin () : Plugin {
@@ -14,3 +14,17 @@ function postBuildPlugin () : Plugin {
         catch { console.warn( `⚠ Pre-render skipped!` ) }
     } };
 }
+
+export default defineConfig ( () => {
+    return {
+        plugins: [ react(), tailwindcss(), postBuildPlugin() ],
+        resolve: { alias: { '@': resolve( __dirname, '.' ) } },
+        build: {
+            cssCodeSplit: true,
+            rollupOptions: { output: { manualChunks ( id ) {
+                if ( id.includes( 'node_modules' ) ) return 'vendor';
+            } } },
+            chunkSizeWarningLimit: 800
+        }
+    };
+} );
