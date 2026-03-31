d79d202e2371ff5ff8219c03d8ebf699a40348f3
komed3 <pk97@outlook.de>
2026-04-01 00:17:35 +0200

    define menu links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { Menu, X } from 'lucide-react';
+import { SiGithub, SiX } from '@icons-pack/react-simple-icons';
+import { Atom, CircleDollarSign, Coffee, Database, Mail, Menu, Package, Plane, X } from 'lucide-react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,6 +8,21 @@ import { Button } from './ui/Button';
 
 export function Header () {
     const [ isOpen, setIsOpen ] = useState( false );
+
+    const menuItems = [
+        { name: 'GitHub', icon: <SiGithub size={20} />, url: 'https://github.com/komed3' },
+        { name: 'Periodic Table', icon: <Atom size={20} />, url: 'https://pse-info.de' },
+        { name: 'AirportMap', icon: <Plane size={20} />, url: 'https://airportmap.de' },
+        { name: 'Realtime Billionaires', icon: <CircleDollarSign size={20} />, url: 'https://realtimebillionaires.de' },
+        { name: 'NPM Packages', icon: <Package size={20} />, url: 'https://npmjs.com/~komed3' },
+        { name: 'APT Repo', icon: <Database size={20} />, url: 'https://deb.komed3.de' }
+    ];
+
+    const socialItems = [
+        { name: 'Ko-fi', icon: <Coffee size={20} />, url: 'https://ko-fi.com/komed3' },
+        { name: 'Twitter/X', icon: <SiX size={20} />, url: 'https://x.com/komed3dev' },
+        { name: 'Contact', icon: <Mail size={20} />, url: 'mailto:webmaster@komed3.de' }
+    ];
 
     return ( <>
         {/** Header */}
