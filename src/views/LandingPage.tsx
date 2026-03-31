c9ce93b7e85ad365a002eec49d60ba092be943f7
komed3 <pk97@outlook.de>
2026-04-01 01:40:56 +0200

    add project grid

--- src/views/LandingPage.tsx
+++ src/views/LandingPage.tsx
@@ -1,10 +1,12 @@
 import { Intro } from '../components/Intro';
+import { ProjectGrid } from '../components/ProjectGrid';
 import { Skills } from '../components/Skills';
 
 
 export function LandingPage () {
     return ( <>
         <Intro />
+        <ProjectGrid />
         <Skills />
     </> );
 }
