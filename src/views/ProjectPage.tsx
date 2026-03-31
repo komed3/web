d5392e1996b3bdf36dbe755e6ae56ecabab053ea
komed3 <pk97@outlook.de>
2026-03-31 02:54:40 +0200

    prep project page

--- src/views/ProjectPage.tsx
+++ src/views/ProjectPage.tsx
@@ -1,3 +1,14 @@
+import { useMemo } from 'react';
+import { Link, useParams, useLocation } from 'react-router-dom';
+
+import { getReadme } from '../services/github';
+
 export function ProjectPage () {
+    const { repoName } = useParams< { repoName: string } >();
+    const { pathname } = useLocation();
+
+    // Memoize readme content to prevent recalculation
+    const readme = useMemo( () => ( repoName ? getReadme( repoName ) : '' ), [ repoName ] );
+
     return ( <></> );
 }
