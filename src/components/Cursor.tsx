d11f0eca1dff4bd8ca41ac491c8c7b28476d3d9d
komed3 <pk97@outlook.de>
2026-03-31 23:17:00 +0200

    Update Cursor.tsx

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -57,7 +57,7 @@ export default function Cursor () {
             } } />
 
             {/** Cursor */}
-            <div className="absolute w-12 h-12 flex items-center justify-center" style={ {
+            <div className="absolute w-12 h-12 flex items-center justify-center mix-blend-difference" style={ {
                 left: 0, top: 0, transform: `translate3d(${mousePos.x}px, ${mousePos.y}px, 0) translate(-50%, -50%)`
             } } >
                 <motion.div className="absolute inset-0" animate={ {
