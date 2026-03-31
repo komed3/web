fed029530bbfa110984d3a0f0419281a89f8c814
komed3 <pk97@outlook.de>
2026-03-31 16:23:24 +0200

    restart whole project

--- src/lib/scrollToTop.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-
-
-export function ScrollToTop () {
-    useEffect( () => window.scrollTo( 0, 0 ), [ useLocation() ] );
-    return null;
-}
