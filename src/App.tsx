37a60225495ce4bf1b75bbe6b8474fa384649094
komed3 <pk97@outlook.de>
2026-04-01 00:47:33 +0200

    add main content section

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Footer } from './components/Footer';
 import { Header } from './components/Header';
 import { Cursor } from './effects/Cursor';
 import { ScrollToTop } from './effects/ScrollTop';
+import { LandingPage } from './views/LandingPage';
+import { ProjectPage } from './views/ProjectPage';
 
 
 export default function App () {
@@ -14,6 +16,14 @@ export default function App () {
             <ScrollToTop />
 
             <Header />
+            <main className="flex-1 pt-20">
+                <AnimatePresence mode="wait">
+                    <Routes>
+                        <Route path="/" element={ <LandingPage /> } />
+                        <Route path="/project/:id" element={ <ProjectPage /> } />
+                    </Routes>
+                </AnimatePresence>
+            </main>
             <Footer />
         </div>
     </Router> );
