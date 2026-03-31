0ca760428710fbb2bf6efe6ae727bdcdf415ab37
komed3 <pk97@outlook.de>
2026-03-31 23:47:26 +0200

    add scroll top effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { AnimatePresence } from 'motion/react';
-import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { ScrollToTop } from './effects/ScrollTop';
 
 
 export default function App () {
     return ( <Router>
         <div className="min-h-screen w-full flex flex-col selection:bg-black selection:text-white relative">
+            <ScrollToTop />
         </div>
     </Router> );
 }
