eacfa7a7a6c973fe962bc83b504bd3eda05d2b3c
komed3 <pk97@outlook.de>
2026-04-01 11:29:35 +0200

    use tailwind config to fix color problem

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                brutalYellow: 'var(--color-brutal-yellow)',
+                brutalGreen: 'var(--color-brutal-green)',
+                brutalBlue: 'var(--color-brutal-blue)',
+                brutalPink: 'var(--color-brutal-pink)',
+                brutalOrange: 'var(--color-brutal-orange)',
+            }
+        }
+    }
+};
