ac6e300ffebe893d280dd8ad060b42754f570356
komed3 <webmaster@komed3.de>
2026-08-24 18:23:39 +0200

    Create Header.tsx

---
+++ src/layout/Header.tsx
@@ -0,0 +1,13 @@
+import { MenuIcon } from 'lucide-react';
+
+export function Header () {
+  return (
+    <header className= ''>
+      <div className= ''>
+        <button>
+          <MenuIcon size= { 32 } strokeWidth= { 0.5 } />
+        </button>
+      </div>
+    </header>
+  );
+}
