1c5ca8baf10559c2e3cb39f51b6934032f0cbe44
komed3 <webmaster@komed3.de>
2026-08-24 23:44:56 +0200

    Update Header.tsx

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,16 @@
+import { Link } from 'react-router';
+
 export function Header () {
   return (
-    <header></header>
+    <header className= 'sticky top-0 h-24'>
+      <div className= ''>
+        <Link
+          to= '/'
+          className= 'font-display text-4xl'
+        >
+          komed3
+        </Link>
+      </div>
+    </header>
   );
 }
