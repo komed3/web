43ca4efd9ef5b36a2428fdb2addeeb5e01d500a2
komed3 <webmaster@komed3.de>
2026-08-24 18:49:59 +0200

    Create Header.tsx

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,13 +1,15 @@
 import { MenuIcon } from 'lucide-react';
 
+
 export function Header () {
   return (
-    <header className= ''>
-      <div className= ''>
-        <button>
-          <MenuIcon size= { 32 } strokeWidth= { 0.5 } />
-        </button>
-      </div>
+    <header className= 'grid grid-cols-[16rem_1fr_6rem] w-full h-24 divide-x divide-slate-200 bg-white'>
+      <button className= 'flex items-center gap-4 w-full h-full p-8'>
+        <MenuIcon size= { 32 } strokeWidth= { 0.5 } />
+        <span>MENU</span>
+      </button>
+      <div className= ''></div>
+      <div className= ''></div>
     </header>
   );
 }
