e9e917ec61552638df59393d7e6b6004988ab9c6
komed3 <webmaster@komed3.de>
2026-08-24 22:13:15 +0200

    remove all

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,15 +1,5 @@
-import { MenuIcon } from 'lucide-react';
-
-
 export function Header () {
   return (
-    <header className= 'grid grid-cols-[16rem_1fr_6rem] w-full h-24 divide-x divide-slate-200 bg-white'>
-      <button className= 'flex items-center gap-4 w-full h-full p-8'>
-        <MenuIcon size= { 32 } strokeWidth= { 0.5 } />
-        <span>MENU</span>
-      </button>
-      <div className= ''></div>
-      <div className= ''></div>
-    </header>
+    <header></header>
   );
 }
