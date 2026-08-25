90ad5dcdcc17211de04eeb91d81b02b55b653dd2
komed3 <webmaster@komed3.de>
2026-08-25 12:51:18 +0200

    prepare header row

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,6 +5,10 @@ export function Header () {
   const [ menuOpen, setMenuOpen ] = useState( false )
 
   return (
-    <></>
+    <>
+      <header className= 'fixed inset-x-0 top-0 z-90 bg-white border-b border-black'>
+        <div className= 'grid h-20 grid-cols-[1fr_auto_1fr] items-stretch'></div>
+      </header>
+    </>
   );
 }
