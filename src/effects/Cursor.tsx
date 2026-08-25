17b464a8e6d502c379dae92b44f02a13b63b739b
komed3 <webmaster@komed3.de>
2026-08-25 23:45:15 +0200

    handle mouse enter/leave

--- src/effects/Cursor.tsx
+++ src/effects/Cursor.tsx
@@ -42,5 +42,18 @@ export function Cursor () {
       setIsVisible( true );
       setIsInteractive( !! ( target as HTMLElement ).closest( 'a, button' ) );
     };
+
+    const handleMouseLeave = () => setIsVisible( false );
+    const handleMouseEnter = () => setIsVisible( true );
+
+    window.addEventListener( 'mousemove', handleMouseMove );
+    document.addEventListener( 'mouseleave', handleMouseLeave );
+    document.addEventListener( 'mouseenter', handleMouseEnter );
+
+    return () => {
+      window.removeEventListener( 'mousemove', handleMouseMove );
+      document.removeEventListener( 'mouseleave', handleMouseLeave );
+      document.removeEventListener( 'mouseenter', handleMouseEnter );
+    };
   }, [ canUseCustomCursor, x, y ] );
 }
