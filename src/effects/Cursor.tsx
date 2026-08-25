e60313de0f24792e2b9b9d5cba734911aa520c79
komed3 <webmaster@komed3.de>
2026-08-25 23:43:29 +0200

    use effect to determine if cursor should be visible

--- src/effects/Cursor.tsx
+++ src/effects/Cursor.tsx
@@ -1,5 +1,5 @@
 import { useMotionValue, useSpring } from 'motion/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export function Cursor () {
@@ -12,4 +12,20 @@ export function Cursor () {
 
   const circleX = useSpring( x, { stiffness: 900, damping: 45, mass: 0.2 } );
   const circleY = useSpring( y, { stiffness: 900, damping: 45, mass: 0.2 } );
+
+  useEffect( () => {
+    const finePointer = matchMedia( '(pointer: fine)' );
+    const hoverPointer = matchMedia( '(hover: hover)' );
+
+    const update = () => setCanUseCustomCursor( finePointer.matches && hoverPointer.matches );
+    update();
+
+    finePointer.addEventListener( 'change', update );
+    hoverPointer.addEventListener( 'change', update );
+
+    return () => {
+      finePointer.removeEventListener( 'change', update );
+      hoverPointer.removeEventListener( 'change', update );
+    };
+  }, [] );
 }
