4be02b4ed581a01a0aaa2276afba5e067acf02c9
komed3 <webmaster@komed3.de>
2026-08-25 19:56:43 +0200

    add page title

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router';
+
 
 export function Header () {
   const [ menuOpen, setMenuOpen ] = useState( false );
@@ -14,7 +15,18 @@ export function Header () {
 
   return (
     <>
-      <header className= 'fixed inset-x-0 top-0 z-98 bg-white'></header>
+      { /** Header */ }
+      <header className= 'fixed inset-x-0 top-0 z-98 bg-white'>
+        <div className= 'flex justify-between items-center h-24 px-7 md:px-12 lg:px-16'>
+          { /** Title */ }
+          <Link
+            to= '/'
+            className= 'font-display text-2xl font-800 tracking-wider'
+          >
+            komed3
+          </Link>
+        </div>
+      </header>
     </>
   );
 }
