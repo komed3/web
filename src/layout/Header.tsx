0404b3f417ac37ed0cb31c7861cc0b7662c23731
komed3 <webmaster@komed3.de>
2026-08-25 13:00:50 +0200

    add header title

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router';
 
 
 export function Header () {
@@ -7,7 +8,20 @@ export function Header () {
   return (
     <>
       <header className= 'fixed inset-x-0 top-0 z-90 bg-white border-b border-black'>
-        <div className= 'grid h-20 grid-cols-[1fr_auto_1fr] items-stretch'></div>
+        <div className= 'grid h-20 grid-cols-[1fr_auto_1fr] items-stretch'>
+
+          { /** Title */ }
+          <div className= 'flex items-center px-5 border-r border-black'>
+            <Link
+              to= '/'
+              className= 'group flex items-center font-display text-4xl leading-none'
+              aria-label= 'komed3 home'
+            >
+              <span className= 'text-brutal-blue'>k</span>omed3
+            </Link>
+          </div>
+
+        </div>
       </header>
     </>
   );
