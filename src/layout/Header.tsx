4647251298ab773961b1d53acfbe881c00afe2f7
komed3 <webmaster@komed3.de>
2026-08-25 19:53:26 +0200

    prepare effects

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,1 +1,14 @@
-export function Header () {}
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router';
+
+export function Header () {
+  const [ menuOpen, setMenuOpen ] = useState( false );
+  const location = useLocation();
+
+  useEffect( () => setMenuOpen( false ), [ location.pathname ] );
+
+  useEffect( () => {
+    document.body.style.overflow = menuOpen ? 'hidden' : '';
+    return () => { document.body.style.overflow = '' };
+  }, [ menuOpen ] );
+}
