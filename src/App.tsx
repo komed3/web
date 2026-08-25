be3e0014a0117aa2e3f2a662c374a91a736d7da8
komed3 <webmaster@komed3.de>
2026-08-25 10:21:05 +0200

    remove all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { Cursor } from './effects/Cursor';
-import { Header } from './layout/Header';
-import { Intro } from './layout/Intro';
-
-
 export default function App () {
   return (
     <>
-      <Cursor />
-      <Intro />
-      <Header />
-
-      <div className= 'h-600' />
+      <div className= 'h-screen' />
     </>
   );
 }
