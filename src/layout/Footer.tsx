098758bbd087fc524d8a0bcb64726a223a55c4ce
komed3 <webmaster@komed3.de>
2026-08-26 02:54:20 +0200

    add workmark within footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,5 +1,13 @@
 export function Footer () {
   return (
-    <></>
+    <footer className= 'bg-white border-t border-black'>
+      { /** Wordmark */ }
+      <div className= 'p-7 md:p-12 lg:p-16 pt-20 md:pt-28 lg:pt-36'>
+        <div className= 'font-display text-right text-9xl font-800 tracking-wider'>
+          <span className= 'text-brutal-blue'>k</span>
+          <span>omed3</span>
+        </div>
+      </div>
+    </footer>
   );
 }
