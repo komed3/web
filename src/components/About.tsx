1dfdbd7ed3670ac14d8e45967433571447adc22c
komed3 <webmaster@komed3.de>
2026-08-27 00:40:03 +0200

    add statement

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,12 @@
 export function About () {
   return (
-    <div className= ''></div>
+    <div className= 'px-12 py-40'>
+      { /** Statement */ }
+      <div className= 'flex flex-col text-7xl uppercase font-black tracking-tight leading-[0.9]'>
+        <span>Building</span>
+        <span>free & open</span>
+        <span>source</span>
+      </div>
+    </div>
   );
 }
