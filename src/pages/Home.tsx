734e310da6a6b34c3947bd907f2affe4437ae5f1
komed3 <webmaster@komed3.de>
2026-08-26 10:37:00 +0200

    add content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,14 @@ export function Home () {
     <>
       { /** Intro */ }
       <div className= 'relative flex flex-col justify-center gap-8 h-page p-12 pb-36 bg-white'>
-        
+        <h1 className= 'max-w-3xl text-7xl uppercase font-bold'>
+          Building free & open-source.
+        </h1>
+
+        <p className= 'max-w-3xl uppercase text-3xl'>
+          Hi, I'm a freelance software developer focusing on Node.js, data visualization, and
+          modern web technology.
+        </p>
       </div>
     </>
   );
