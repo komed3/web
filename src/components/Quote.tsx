ce7a9464d853462d025cc1f0b4f8c708721d1d06
komed3 <webmaster@komed3.de>
2026-08-26 20:03:10 +0200

    Update Quote.tsx

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,13 +1,8 @@
 export function Quote () {
   return (
-    <div className= ''>
-      <div className= ''>
-        Leave the road,
-      </div>
-
-      <div className= ''>
-        take the trails.
-      </div>
+    <div className= 'flex flex-col items-center p-20 text-7xl uppercase font-extralight tracking-[2rem]'>
+      <span className= 'pr-50'>Leave the road,</span>
+      <span className= 'pl-50'>take the trails.</span>
     </div>
   );
 }
