4fca254d23cd76536f891e722371b230378f5b46
komed3 <webmaster@komed3.de>
2026-08-26 23:38:42 +0200

    add footer logo

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,7 +1,23 @@
+import { Link } from 'react-router';
+
+import { Logo } from '../components/Logo';
+
+
 export function Footer () {
   return (
-    <footer className= 'mx-12 text-(--bg) bg-(--text)'>
-      ...
+    <footer className= 'mx-12 p-12 text-(--bg) bg-(--text)'>
+      { /** Meta */ }
+      <div className= 'grid grid-cols-2 items-end'>
+        { /** Logo */ }
+        <div>
+          <Link
+            to= '/'
+            aria-label= 'komed3'
+          >
+            <Logo className= 'w-48 h-auto' />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
