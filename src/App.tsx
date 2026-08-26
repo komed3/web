86078c362a6b3c1a7b108ecab03d7608e8b7306e
komed3 <webmaster@komed3.de>
2026-08-26 10:26:20 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import { Route, Routes } from 'react-router';
+
 import { Cursor } from './effects/Cursor';
 import { Footer } from './layout/Footer';
 import { Header } from './layout/Header';
+import { Home } from './pages/Home';
 
 
 export default function App () {
@@ -8,7 +11,11 @@ export default function App () {
     <>
       <Header />
 
-      <main className= 'min-h-screen' />
+      <main className= 'mt-24 min-h-screen'>
+        <Routes>
+          <Route path= '/' element= { <Home /> } />
+        </Routes>
+      </main>
 
       <Footer />
       <Cursor />
