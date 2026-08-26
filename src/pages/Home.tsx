e66ffc489e768089edeae65f3c69bd7e18b6a8bf
komed3 <webmaster@komed3.de>
2026-08-26 19:27:41 +0200

    make intro it's own component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,10 @@
+import { Intro } from '../components/Intro';
+
+
 export function Home () {
   return (
     <>
-      { /** Intro */ }
-      <div className= 'relative w-full h-screen'>
-        <div className= 'centered pt-10'>
-          <p className= 'max-w-6xl text-5xl uppercase font-extralight tracking-widest leading-snug'>
-            Hi, I'm <b>komed3</b>, a freelance <b>software</b> developer focusing
-            on <b>Node.js</b>, data visualization, and <b>modern web</b> technology.
-          </p>
-        </div>
-      </div>
+      <Intro />
     </>
   );
 }
