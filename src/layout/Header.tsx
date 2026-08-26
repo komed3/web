f0baa891070186aceae1a113a38f0a33be147045
komed3 <webmaster@komed3.de>
2026-08-26 15:26:45 +0200

    add header logo "k"

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router';
+
+
 export function Header () {
   return (
-    <header className= ''>
-      //
+    <header className= 'fixed inset-x-0 top-0 h-24'>
+      { /** Logo */ }
+      <div className= 'centered'>
+        <Link
+          className= 'inline-flex justify-center items-center w-20 h-20 font-display text-4xl'
+          to= '/'
+        >
+          <span>k</span>
+        </Link>
+      </div>
     </header>
   );
 }
