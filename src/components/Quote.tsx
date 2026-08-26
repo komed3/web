b7dae4e7a8c8ab0aab1fa28b66137bc9cd33b754
komed3 <webmaster@komed3.de>
2026-08-27 00:13:02 +0200

    add more padding

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,6 +1,6 @@
 export function Quote () {
   return (
-    <div className= 'flex flex-col items-center p-20 text-7xl uppercase font-extralight tracking-[2rem]'>
+    <div className= 'flex flex-col items-center px-20 py-32 text-7xl uppercase font-extralight tracking-[2rem]'>
       <span className= 'pr-50'>Leave the road,</span>
       <span className= 'pl-50'>take the trails.</span>
     </div>
