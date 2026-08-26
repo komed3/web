134b16a8c0b33c525ebdc9a57c7c3e6ef9748721
komed3 <webmaster@komed3.de>
2026-08-27 00:42:49 +0200

    about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,7 +9,21 @@ export function About () {
       </div>
 
       { /** About */ }
-      <div className= 'grid grid-cols-2 gap-24 mt-40'></div>
+      <div className= 'grid grid-cols-2 gap-24 mt-40'>
+        <div className= 'space-y-8 text-3xl font-extralight leading-relaxed'>
+          <p>
+            I'm a freelance software developer focusing on Node.js packages,
+            data visualization, and contemporary web development.
+          </p>
+
+          <p>
+            I've always been drawn to understanding how things work. My
+            interest in electronics started around 2010 and eventually grew
+            into a fascination with software, systems, and the things that
+            can be built with them.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
