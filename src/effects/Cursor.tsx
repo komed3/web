c52b3f209450d5eced28825009ee2ba5098ecd98
komed3 <webmaster@komed3.de>
2026-08-26 19:16:06 +0200

    prepare cursor movement

--- src/effects/Cursor.tsx
+++ src/effects/Cursor.tsx
@@ -1,7 +1,18 @@
-import { AnimatePresence } from 'motion/react';
+import { AnimatePresence, useMotionValue, useSpring } from 'motion/react';
+import { useState } from 'react';
 
 
 export function Cursor () {
+  const [ canUseCustomCursor, setCanUseCustomCursor ] = useState( false );
+  const [ isVisible, setIsVisible ] = useState( false );
+  const [ isInteractive, setIsInteractive ] = useState( false );
+
+  const x = useMotionValue( 0 );
+  const y = useMotionValue( 0 );
+
+  const circleX = useSpring( x, { stiffness: 900, damping: 45, mass: 0.2 } );
+  const circleY = useSpring( y, { stiffness: 900, damping: 45, mass: 0.2 } );
+
   return (
     <AnimatePresence></AnimatePresence>
   );
