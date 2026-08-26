a1f4e2014ba541c60baefb01f4686aceb7521bd5
komed3 <webmaster@komed3.de>
2026-08-27 00:44:31 +0200

    add philosophy section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,8 +8,9 @@ export function About () {
         <span>source</span>
       </div>
 
-      { /** About */ }
+      { /** Content */ }
       <div className= 'grid grid-cols-2 gap-24 mt-40'>
+        { /** About */ }
         <div className= 'space-y-8 text-3xl font-extralight leading-relaxed'>
           <p>
             I'm a freelance software developer focusing on Node.js packages,
@@ -21,6 +22,23 @@ export function About () {
             interest in electronics started around 2010 and eventually grew
             into a fascination with software, systems, and the things that
             can be built with them.
+          </p>
+        </div>
+
+        { /** Philosophy */ }
+        <div className= 'flex flex-col justify-end gap-8'>
+          <div className= 'text-xs uppercase font-extralight tracking-[0.4rem]'>
+            Philosophy
+          </div>
+
+          <p className= 'text-2xl font-extralight leading-relaxed'>
+            I believe in the power of open collaboration. My stack is
+            intentionally built around open-source tools to keep things
+            transparent, accessible, and built to last.
+          </p>
+
+          <p className= 'text-2xl font-extralight leading-relaxed'>
+            Open source isn't just a choice. It's a standard.
           </p>
         </div>
       </div>
