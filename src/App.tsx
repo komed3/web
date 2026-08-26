e5df8128618e94aded76ec58d8e2af06ea673fcb
komed3 <webmaster@komed3.de>
2026-08-26 19:07:35 +0200

    set color vars based on path

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
+import { useEffect } from 'react';
 import { Route, Routes, useLocation } from 'react-router';
 
 import { Header } from './layout/Header';
 import { Home } from './pages/Home';
 
 
-const COLORS = {
-  '/': 'text-black bg-white',
-  '/stack': 'text-white bg-brutal-blue',
-  '/index': 'text-white bg-black'
+const COLOR_VARS = {
+  '/': { '--bg': '#fff', '--text': '#000' },
+  '/stack': { '--bg': '#1e40af', '--text': '#fff' },
+  '/index': { '--bg': '#000', '--text': '#fff' }
 } as const;
 
 
 export default function App () {
   const { pathname } = useLocation();
 
+  useEffect( () => {
+    const vars = COLOR_VARS[ pathname as keyof typeof COLOR_VARS ] || COLOR_VARS[ '/' ];
+    Object.entries( vars ).forEach( ( [ key, value ] ) =>
+      document.documentElement.style.setProperty( key, value )
+    );
+  }, [ pathname ] );
+
   return (
-    <div className= { ( ( COLORS as any )[ pathname ] ?? '' ) }>
+    <div className= 'bg-(--bg) text-(--text) transition-colors duration-300'>
       <Header />
 
       <main className= 'min-h-screen'>
