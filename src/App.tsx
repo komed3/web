e893a4abd02147e51212980613fa74b4711f9f13
komed3 <webmaster@komed3.de>
2026-08-26 15:39:55 +0200

    mount home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from 'react-router';
+
 import { Header } from './layout/Header';
+import { Home } from './pages/Home';
 
 
 export default function App () {
   return (
-    <>
+    <div className= ''>
       <Header />
-    </>
+
+      <main className= 'min-h-screen'>
+        <Routes>
+          <Route path= '/' element= { <Home /> } />
+        </Routes>
+      </main>
+    </div>
   );
 }
