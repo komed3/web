859cf21c477e5b9ea3857716473746a9cfad92d2
komed3 <webmaster@komed3.de>
2026-08-26 15:49:17 +0200

    add home intro

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,14 @@ export function Home () {
   return (
     <>
       { /** Intro */ }
-      <div className= 'w-full h-screen'></div>
+      <div className= 'relative w-full h-screen'>
+        <div className= 'centered'>
+          <p className= 'max-w-6xl text-5xl uppercase font-extralight tracking-widest leading-snug'>
+            Hi, I'm <b>komed3</b>, a freelance <b>software</b> developer focusing
+            on <b>Node.js</b>, data visualization, and <b>modern web</b> technology.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
