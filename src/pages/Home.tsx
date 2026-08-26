02cf025c5e0c406358e860199f5790b0bed2f0f1
komed3 <webmaster@komed3.de>
2026-08-26 10:36:41 +0200

    create full size intro section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,12 @@
+import { Mouse } from 'lucide-react';
+
+
 export function Home () {
   return (
     <>
       { /** Intro */ }
-      <div className= 'h-page'>
-        ...
+      <div className= 'relative flex flex-col justify-center gap-8 h-page p-12 pb-36 bg-white'>
+        
       </div>
     </>
   );
