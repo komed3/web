ac9aa78f596c7866daf5ff9582c5fdae0ac2ba32
komed3 <webmaster@komed3.de>
2026-08-26 03:01:58 +0200

    add footer meta (copyright / build info)

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -8,6 +8,23 @@ export function Footer () {
           <span>omed3</span>
         </div>
       </div>
+
+      { /** Meta */ }
+      <div
+        className= {
+          'flex flex-col md:flex-row md:justify-between md:items-center gap-4 px-7 md:px-12 lg:px-16 ' +
+          'py-6 font-mono text-[11px] uppercase tracking-[0.18em] border-t border-black'
+        }
+      >
+        { /** Copyright */}
+        <span>Copyright { new Date().getFullYear() } by komed3</span>
+
+        { /** Build Info */ }
+        <div className= 'flex gap-6'>
+          <span>Build { process.env.VITE_BUILD_ID ?? 'XXXXXXXX-XXXX' }</span>
+          <span>Commit { process.env.VITE_COMMIT_SHA ?? 'XXXXXXX' }</span>
+        </div>
+      </div>
     </footer>
   );
 }
