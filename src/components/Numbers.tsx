411d9089a9f80b5c1f683e458f4c0dc14619ca54
komed3 <webmaster@komed3.de>
2026-08-27 01:00:59 +0200

    add label

--- src/components/Numbers.tsx
+++ src/components/Numbers.tsx
@@ -1,5 +1,12 @@
 export function Numbers () {
   return (
-    <div className= 'flex gap-12 p-12'></div>
+    <div className= 'flex p-12 divide-x divide-(--text)'>
+      { /** Label */ }
+      <div className= 'shrink-0 flex flex-col pr-12 text-2xl uppercase font-extralight tracking-widest'>
+        <span>By the</span>
+        <span>numbers</span>
+        <span>_</span>
+      </div>
+    </div>
   );
 }
