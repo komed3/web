c21d0a4d24e4624bdd9146f9967f0055e97c8187
komed3 <webmaster@komed3.de>
2026-08-27 00:41:28 +0200

    prepare about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,6 +7,9 @@ export function About () {
         <span>free & open</span>
         <span>source</span>
       </div>
+
+      { /** About */ }
+      <div className= 'grid grid-cols-2 gap-24 mt-40'></div>
     </div>
   );
 }
