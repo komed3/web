95e3cabd92d8741c069c1bf25577ff5db6047ec8
komed3 <webmaster@komed3.de>
2026-08-27 11:11:21 +0200

    define skillset

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,3 +1,11 @@
+import { SiTypescript } from 'react-icons/si';
+
+
+const SKILLS = [
+  { label: 'Typescript', icon: SiTypescript }
+] as const;
+
+
 export function Skills () {
   return (
     <div className= 'grid grid-cols-4 divide-x divide-y divide-(--text)'></div>
