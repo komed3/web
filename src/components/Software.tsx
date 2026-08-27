090e6c14f54bdf2cee76d1294e65d7fe3c68f0f6
komed3 <webmaster@komed3.de>
2026-08-27 16:03:45 +0200

    add software section label

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -1,5 +1,12 @@
 export function Software () {
   return (
-    <></>
+    <div className= 'py-16 text-(--contrast) bg-(--main)'>
+      { /** Label */ }
+      <div className= 'px-12 mb-32'>
+        <div className= 'text-sm uppercase tracking-[0.3em]'>
+          Software
+        </div>
+      </div>
+    </div>
   );
 }
