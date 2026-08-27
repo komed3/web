75c49b78ad55f6d437437948ed3df28a8e1c4144
komed3 <webmaster@komed3.de>
2026-08-27 13:04:23 +0200

    add intro transition effect

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,12 +1,22 @@
+import { AnimatePresence, motion } from 'motion/react';
+
+
 export function Intro () {
   return (
     <div className= 'relative w-full h-screen'>
-      <div className= 'centered pt-10'>
-        <p className= 'max-w-6xl text-5xl uppercase font-extralight tracking-widest leading-snug'>
-          Hi, I'm <b>komed3</b>, a freelance <b>software</b> developer focusing
-          on <b>Node.js</b>, data visualization, and <b>modern web</b> technology.
-        </p>
-      </div>
+      <AnimatePresence>
+        <motion.div
+          initial= { { y: 200, opacity: 0 } }
+          whileInView= { { y: 0, opacity: 1 } }
+          viewport= { { once: true, amount: 0.3 } }
+          className= 'centered pt-10'
+        >
+          <p className= 'max-w-6xl text-5xl uppercase font-extralight tracking-widest leading-snug'>
+            Hi, I'm <b>komed3</b>, a freelance <b>software</b> developer focusing
+            on <b>Node.js</b>, data visualization, and <b>modern web</b> technology.
+          </p>
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
