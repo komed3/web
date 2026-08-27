3d8a77296aa4c51ebc8c8576dfd3fcabac9fd46f
komed3 <webmaster@komed3.de>
2026-08-27 23:22:09 +0200

    implement (project) index page

---
+++ src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import { Featured } from '../components/Featured';
+import { Projects } from '../components/Projects';
+
+
+export function Index () {
+  return (
+    <>
+      <Featured />
+      <Projects />
+    </>
+  );
+}
