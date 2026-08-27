f9ab62295a0ada96c94be01721eb4d88e48225f6
komed3 <webmaster@komed3.de>
2026-08-27 16:05:18 +0200

    add big section header

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -6,6 +6,10 @@ export function Software () {
         <div className= 'text-sm uppercase tracking-[0.3em]'>
           Software
         </div>
+
+        <div className= 'mt-6 -ml-1.5 text-6xl font-extralight'>
+          Built with open tools
+        </div>
       </div>
     </div>
   );
