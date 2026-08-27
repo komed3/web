2fca68a87017e71db7524c9fd02107ce03d20266
komed3 <webmaster@komed3.de>
2026-08-27 11:14:50 +0200

    implement skills items

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -8,6 +8,21 @@ const SKILLS = [
 
 export function Skills () {
   return (
-    <div className= 'grid grid-cols-4 divide-x divide-y divide-(--text)'></div>
+    <div className= 'grid grid-cols-4 py-40 divide-x divide-y divide-(--text)'>
+      { SKILLS.map( ( { label, icon: Icon } ) => (
+        <div
+          key= { label }
+          className= 'p-12'
+        >
+          <div className= 'flex justify-end mb-16'>
+            <Icon size= { 64 } />
+          </div>
+
+          <div className= 'text-2xl uppercase font-extralight tracking-widest'>
+            { label }
+          </div>
+        </div>
+      ) ) }
+    </div>
   );
 }
