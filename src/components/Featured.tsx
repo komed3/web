c2c3155ad32408446f9abdb148110bdfd90c4370
komed3 <webmaster@komed3.de>
2026-08-27 23:44:04 +0200

    add featured projects

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -2,8 +2,20 @@ const PROJECTS = [ {
   uri: 'airportmap',
   name: 'Airportmap',
   category: 'Data / Web',
-  description: 'The free and open source airport database',
+  description: 'The free and open-source airport database',
   link: 'airportmap.de'
+}, {
+  uri: 'pseinfo',
+  name: 'Periodic Table',
+  category: 'Data / Education',
+  description: 'Interactive open-source periodic table',
+  link: 'pse-info.de',
+}, {
+  uri: 'rtbnext',
+  name: 'RTBNext',
+  category: 'API / Data',
+  description: 'Tracking the wealthiest people on the planet',
+  url: 'rtbnext.de'
 } ] as const;
 
 
