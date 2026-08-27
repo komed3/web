371b4f987f8d325aa0be5f9640eacd0b8ca1ded2
komed3 <webmaster@komed3.de>
2026-08-27 16:07:26 +0200

    declare used software

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -1,3 +1,26 @@
+import { SiGimp, SiGit, SiGithub, SiInkscape, SiKrita } from 'react-icons/si';
+import { VscVscodeInsiders } from 'react-icons/vsc';
+
+
+const SOFTWARE = [ {
+  label: 'Visual Studio Code',
+  desc: 'Development environment',
+  icons: [ VscVscodeInsiders ]
+}, {
+  label: 'Git / GitHub',
+  desc: 'Version control',
+  icons: [ SiGit, SiGithub ]
+}, {
+  label: 'Inkscape',
+  desc: 'Vector graphics',
+  icons: [ SiInkscape ]
+}, {
+  label: 'Krita / Gimp',
+  desc: 'Digital graphics',
+  icons: [ SiKrita, SiGimp ]
+} ] as const;
+
+
 export function Software () {
   return (
     <div className= 'py-16 text-(--contrast) bg-(--main)'>
@@ -8,9 +31,12 @@ export function Software () {
         </div>
 
         <div className= 'mt-6 -ml-1.5 text-6xl font-extralight'>
-          Built with open tools
+          Built with open tools_
         </div>
       </div>
+
+      { /** Content */ }
+      <div className= 'px-12'></div>
     </div>
   );
 }
