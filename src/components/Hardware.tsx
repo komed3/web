027de2959d2a34242007ebd53f84bdee31402e95
komed3 <webmaster@komed3.de>
2026-08-27 19:44:12 +0200

    declare hardware resources const

--- src/components/Hardware.tsx
+++ src/components/Hardware.tsx
@@ -1,5 +1,30 @@
+import { SiAmd } from 'react-icons/si';
+
+
+const RESOURCES = [ {
+  label: 'Main workstation',
+  icon: SiAmd,
+  title: '5900XT',
+  subtitle: '16C / 32T',
+  items: [
+    [ 'Memory', '64GB DDR4 3200 MT/s' ],
+    [ 'Graphics', 'RTX 3080 Strix 10GB OC' ],
+    [ 'System', 'Windows 11 Home' ]
+  ]
+} ] as const;
+
+
 export function Hardware () {
   return (
-    <div className= 'flex items-stretch py-16'></div>
+    <div className= 'flex items-stretch py-16'>
+      { /** Label */ }
+      <div className= 'shrink-0 flex flex-col px-12 text-2xl uppercase font-extralight tracking-widest'>
+        <span>Hardware</span>
+        <span>resources</span>
+        <span>_</span>
+      </div>
+
+      { /** Resources */ }
+    </div>
   );
 }
