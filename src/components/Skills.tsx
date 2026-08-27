3f2dbb832295958efe11cf0816d3829d34fd128a
komed3 <webmaster@komed3.de>
2026-08-27 11:21:02 +0200

    Update Skills.tsx

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import { SiCplusplus, SiJavascript, SiPhp, SiTypescript } from 'react-icons/si';
+import { SiCplusplus, SiJavascript, SiNodedotjs, SiPhp, SiTypescript } from 'react-icons/si';
 
 
 const SKILLS = [
@@ -12,21 +12,23 @@ const SKILLS = [
 
 export function Skills () {
   return (
-    <div className= 'grid grid-cols-4 py-40 divide-x divide-y divide-(--text)'>
-      { SKILLS.map( ( { label, icon: Icon } ) => (
-        <div
-          key= { label }
-          className= 'p-12'
-        >
-          <div className= 'flex justify-end mb-16'>
-            <Icon size= { 64 } />
-          </div>
+    <div className= 'py-40'>
+      <div className= 'grid grid-cols-4 gap-px bg-(--main)'>
+        { SKILLS.map( ( { label, icon: Icon } ) => (
+          <div
+            key= { label }
+            className= 'p-12 bg-(--accent)'
+          >
+            <div className= 'flex justify-end mb-16'>
+              <Icon size= { 64 } />
+            </div>
 
-          <div className= 'text-2xl uppercase font-extralight tracking-widest'>
-            { label }
+            <div className= 'text-2xl uppercase font-extralight tracking-widest'>
+              { label }
+            </div>
           </div>
-        </div>
-      ) ) }
+        ) ) }
+      </div>
     </div>
   );
 }
