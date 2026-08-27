e9b0ea939e07979801be9644812160c9187a5039
komed3 <webmaster@komed3.de>
2026-08-27 19:06:16 +0200

    Update Software.tsx

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -25,50 +25,44 @@ export function Software () {
   return (
     <div className= 'py-16 text-(--contrast) bg-(--main)'>
       { /** Header */ }
-      <div className= 'px-12 mb-32'>
-        <div className= 'text-sm uppercase tracking-[0.3em]'>
+      <div className= 'px-12'>
+        <div className= 'text-xs uppercase tracking-[0.3em]'>
           Software
         </div>
 
-        <div className= 'mt-6 -ml-1.5 text-6xl font-extralight'>
+        <div className= 'mt-6 -ml-1.5 text-5xl font-extralight'>
           Built with open tools_
         </div>
       </div>
 
       { /** Content */ }
-      <div className= 'px-12'>
-        { SOFTWARE.map( ( { label, desc, icons }, i ) => (
-          <motion.div
-            key= { label }
-            className= 'relative min-h-60 py-12 border-t border-(--contrast)'
-            initial= { { opacity: 0, y: 30 } }
-            whileInView= { { opacity: 1, y: 0 } }
-            transition= { { delay: i * 0.05 } }
-            viewport= { { once: true, amount: 0.3 } }
+      <div className= 'grid grid-cols-2 gap-px mx-12 my-16 bg-(--contrast)'>
+        { SOFTWARE.map( ( { label, desc, icon: Icon }, i ) => (
+          <div
+            key= { i }
+            className= 'flex justify-between items-center gap-12 py-12 bg-(--main)'
+            style= { {
+              paddingLeft: i % 2 == 0 ? 0 : 48,
+              paddingRight: i % 2 == 0 ? 48 : 0
+            } }
           >
-            { /** Description */ }
-            <div>
-              <div className= '-ml-1 text-5xl uppercase font-extralight tracking-widest'>
-                { label }
-              </div>
-
-              <div className= 'mt-4 text-sm uppercase tracking-[0.2em]'>
+            <div className= 'space-y-2'>
+              <div className= 'text-xs uppercase tracking-[0.3em]'>
                 { desc }
               </div>
+
+              <div className= 'text-3xl font-light tracking-tight'>
+                { label }
+              </div>
             </div>
 
-            { /** Icons */ }
-            <div className= 'absolute right-0 bottom-10 flex items-end gap-6'>
-              { icons.map( ( Icon, j ) => (
-                <Icon key= { j } size= { 64 } />
-              ) ) }
-            </div>
-          </motion.div>
+            <Icon size= { 48 } />
+          </div>
         ) ) }
       </div>
 
       { /** Footer */ }
-      <div className= 'px-12 pt-24 text-sm uppercase tracking-[0.3em]'>
+      <div className= 'px-12 text-xs uppercase tracking-[0.3em]'>
         Open source / free software / independent tools
       </div>
     </div>
