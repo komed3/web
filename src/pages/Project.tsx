9522410d1a6754a5011e7978fd639ed385fae83f
komed3 <webmaster@komed3.de>
2026-08-28 18:10:30 +0200

    if project doesn't exists, redirect to index

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,8 +1,18 @@
-import { useParams } from 'react-router';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router';
+
+import projects from '../data/projects.json';
 
 
 export function Project () {
+  const navigate = useNavigate();
+
   const { id } = useParams();
+  const project = projects.find( p => p.id === id );
+
+  useEffect( () => {
+    if ( ! project ) navigate( '/index', { replace: true } );
+  }, [ id, projects, navigate ] );
 
   return (
     <></>
