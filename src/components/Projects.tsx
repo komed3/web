2c086c9b0fb670fc9dcfd593ac4a1d14073cb923
komed3 <webmaster@komed3.de>
2026-08-28 12:47:00 +0200

    minor layout tweaks

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -14,12 +14,13 @@ export function Projects () {
     <div className= 'px-12 py-32'>
       { /** Header */ }
       <div className= 'flex justify-between items-end pb-20'>
-        <div>
+        { /** Title */ }
+        <div className= 'space-y-6'>
           <div className= 'text-xs uppercase tracking-[0.3em]'>
             Selected work
           </div>
 
-          <div className= 'mt-6 -ml-6 text-[clamp(6rem,14vw,13rem)] font-extralight leading-[0.8] tracking-tighter'>
+          <div className= '-ml-6 text-[clamp(6rem,14vw,13rem)] font-extralight leading-[0.8] tracking-tighter'>
             Projects
           </div>
         </div>
