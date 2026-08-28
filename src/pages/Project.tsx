113e26debc60191fcfdc91cfbb23a466bd774a36
komed3 <webmaster@komed3.de>
2026-08-28 23:38:26 +0200

    project header

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -20,6 +20,35 @@ export function Project () {
   );
 
   return project && (
-    <></>
+    <>
+      { /** Project Header */ }
+      <div className= 'grid grid-cols-[1fr_1px_2fr] gap-12 w-full h-screen p-12 pt-36'>
+        { /** Aside */ }
+        <div className= 'flex flex-col items-end'>
+          <div className= 'flex-1' />
+
+          { project.status && (
+            <div className= 'text-(--contrast) bg-(--main)'>
+              { project.status }
+            </div>
+          ) }
+        </div>
+
+        <div className= 'bg-(--main)' />
+
+        { /** Title */ }
+        <div className= 'flex flex-col justify-end'>
+          <div className= '-ml-2 text-[clamp(4rem,8vw,8rem)] font-extralight leading-none tracking-tighter'>
+            { project.title }
+          </div>
+
+          { project.desc && (
+            <p className= 'mt-8 max-w-5xl text-3xl font-light leading-snug'>
+              { project.desc }
+            </p>
+          ) }
+        </div>
+      </div>
+    </>
   );
 }
