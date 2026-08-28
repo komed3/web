a1b8534b3962d61094f53217977a57192796de7b
komed3 <webmaster@komed3.de>
2026-08-28 16:51:59 +0200

    iterate over masonry columns

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -134,6 +134,13 @@ export function Projects () {
           ) ) }
         </div>
       </motion.div>
+
+      { /** Masonry */ }
+      <div className= 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-16'>
+        { masonry.map( ( column, c ) => (
+          <></>
+        ) ) }
+      </div>
     </div>
   );
 }
