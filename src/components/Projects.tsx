6cd1fe2fda8b6938a84d80c881434e4132106f73
komed3 <webmaster@komed3.de>
2026-08-28 12:41:22 +0200

    add projects header

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,18 @@
 export function Projects () {
   return (
-    <></>
+    <div className= 'px-12 py-32'>
+      { /** Header */ }
+      <div className= 'flex justify-between items-end pb-20'>
+        <div>
+          <div className= 'text-xs uppercase tracking-[0.3em]'>
+            Selected work
+          </div>
+
+          <div className= 'mt-6 -ml-6 text-[clamp(6rem,14vw,13rem)] font-extralight leading-[0.8] tracking-tighter'>
+            Projects
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
