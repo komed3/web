5bf26b9430f7457dabcb1485746b560d7a786d7e
komed3 <webmaster@komed3.de>
2026-08-29 15:39:52 +0200

    Update Project.tsx

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -28,17 +28,17 @@ export function Project () {
         { /** Hero */ }
         <div className= 'flex flex-col justify-end p-12'>
           { /** Type */ }
-          <div className= 'mb-6 text-2xl font-light uppercase tracking-[0.3em]'>
+          <div className= 'mb-4 text-2xl font-light uppercase tracking-[0.3em]'>
             { project.type }
           </div>
 
           { /** Title */ }
-          <div className= '-ml-3 text-[clamp(4rem,8vw,8rem)] font-extralight leading-[0.8] -tracking-widest'>
+          <div className= '-ml-3 max-w-4xl text-[clamp(4rem,8vw,8rem)] font-extralight leading-none tracking-tighter'>
             { project.title }
           </div>
 
           { /** Info */ }
-          <div className= 'flex justify-between items-end gap-24 mt-24'>
+          <div className= 'flex justify-between items-end gap-24 mt-20'>
             <div>
               { /** Description */ }
               { project.desc && (
