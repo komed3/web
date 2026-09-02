e445410502a8258128ce13817d98107a0d073f8d
komed3 <webmaster@komed3.de>
2026-09-02 23:44:21 +0200

    prepare cursor calculation

--- src/effects/Cursor.tsx
+++ src/effects/Cursor.tsx
@@ -1,4 +1,18 @@
+import { useMotionValue, useSpring } from 'motion/react';
+import { useState } from 'react';
+
+
 export function Cursor () {
+  const [ canUseCustomCursor, setCanUseCustomCursor ] = useState( false );
+  const [ isVisible, setIsVisible ] = useState( false );
+  const [ isInteractive, setIsInteractive ] = useState( false );
+
+  const x = useMotionValue( 0 );
+  const y = useMotionValue( 0 );
+
+  const circleX = useSpring( x, { stiffness: 900, damping: 45, mass: 0.2 } );
+  const circleY = useSpring( y, { stiffness: 900, damping: 45, mass: 0.2 } );
+
   return (
     <></>
   );
