4fb866c40158b85b9a21ca42ebd44582f6b40918
komed3 <webmaster@komed3.de>
2026-09-02 23:46:05 +0200

    check if cursor should be rendered

--- src/effects/Cursor.tsx
+++ src/effects/Cursor.tsx
@@ -28,6 +28,22 @@ export function Cursor () {
     };
   }, [] );
 
+  useEffect( () => {
+    const finePointer = matchMedia( '(pointer: fine)' );
+    const hoverPointer = matchMedia( '(hover: hover)' );
+
+    const update = () => setCanUseCustomCursor( finePointer.matches && hoverPointer.matches );
+    update();
+
+    finePointer.addEventListener( 'change', update );
+    hoverPointer.addEventListener( 'change', update );
+
+    return () => {
+      finePointer.removeEventListener( 'change', update );
+      hoverPointer.removeEventListener( 'change', update );
+    };
+  }, [] );
+
   return (
     <></>
   );
