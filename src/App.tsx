184692b8f52fd42a47735e7e3050475e4c50433a
komed3 <webmaster@komed3.de>
2026-09-02 22:46:43 +0200

    declare theme colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+const THEMES = {
+  default: { '--accent': '#fff',    '--main': '#000', '--contrast': '#fff' },
+  stack:   { '--accent': '#1e40af', '--main': '#fff', '--contrast': '#000' },
+  project: { '--accent': '#fbbf24', '--main': '#000', '--contrast': '#fff' },
+  index:   { '--accent': '#000',    '--main': '#fff', '--contrast': '#000' }
+} as const;
+
+
 export default function App () {
   return (
     <></>
