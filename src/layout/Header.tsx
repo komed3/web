b177419d142f11494668428ba35d92879fe3f961
komed3 <webmaster@komed3.de>
2026-09-02 23:24:31 +0200

    add header home link

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router';
+
+
 export function Header () {
   return (
-    <header className= 'fixed inset-x-0 top-0 z-90 h-24 bg-(--accent)'></header>
+    <header className= 'fixed inset-x-0 top-0 z-90 h-24 bg-(--accent)'>
+      { /** Logo */ }
+      <div className= 'centered'>
+        <Link
+          className= 'inline-flex justify-center items-center w-20 h-20'
+          to= '/'
+          aria-label= 'komed3'
+        >
+          k
+        </Link>
+      </div>
+    </header>
   );
 }
