d2fd16b954860fb5684d2b1f2cee894f388a50af
komed3 <webmaster@komed3.de>
2026-09-03 13:41:26 +0200

    declare projects

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,3 +1,30 @@
+const PROJECTS = [ {
+  uri: 'airportmap',
+  name: 'Airportmap',
+  category: 'Data / Web',
+  desc: 'The free and open-source airport database',
+  link: 'airportmap.de'
+}, {
+  uri: 'pseinfo',
+  name: 'Periodic Table',
+  category: 'Data / Education',
+  desc: 'Interactive open-source periodic table and chemical database',
+  link: 'pse-info.de'
+}, {
+  uri: 'rtbnext',
+  name: 'RTBNext',
+  category: 'API / Data',
+  desc: 'Open infrastructure for real-time billionaire data',
+  link: 'rtbnext.de'
+}, {
+  uri: 'pulsar',
+  name: 'Pulsar',
+  category: 'Programming',
+  desc: 'Programming language focused on mathematical problems',
+  link: 'github.com/plsrc'
+} ] as const;
+
+
 export function Featured () {
   return (
     <></>
