af05349134a25432e0c3542b0adb5b8de9616679
komed3 <webmaster@komed3.de>
2026-09-03 20:06:22 +0200

    add header

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -1,3 +1,16 @@
 export function Software () {
-  return ();
+  return (
+    <div className= 'my-24 py-12 text-(--contrast) bg-(--main)'>
+      { /** Header */ }
+      <div className= 'px-6 sm:px-12'>
+        <div className= 'text-xs uppercase tracking-[0.3em]'>
+          Software
+        </div>
+
+        <div className= 'mt-6 -ml-1.5 text-4xl sm:text-5xl font-extralight'>
+          Built with open tools_
+        </div>
+      </div>
+    </div>
+  );
 }
