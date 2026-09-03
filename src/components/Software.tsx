53dea1c0490818067c62c68164be600b0d047795
komed3 <webmaster@komed3.de>
2026-09-03 20:06:53 +0200

    declare software items

--- src/components/Software.tsx
+++ src/components/Software.tsx
@@ -1,3 +1,27 @@
+import { motion } from 'motion/react';
+import { SiGit, SiInkscape, SiKrita } from 'react-icons/si';
+import { VscVscodeInsiders } from 'react-icons/vsc';
+
+
+const SOFTWARE = [ {
+  label: 'Visual Studio Code',
+  desc: 'Development environment',
+  icon: VscVscodeInsiders
+}, {
+  label: 'Git / GitHub',
+  desc: 'Version control',
+  icon: SiGit
+}, {
+  label: 'Inkscape',
+  desc: 'Vector graphics',
+  icon: SiInkscape
+}, {
+  label: 'Krita / Gimp',
+  desc: 'Digital graphics',
+  icon: SiKrita
+} ] as const;
+
+
 export function Software () {
   return (
     <div className= 'my-24 py-12 text-(--contrast) bg-(--main)'>
