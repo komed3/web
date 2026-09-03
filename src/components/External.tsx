6e295981c88573d238c3d6f247f86cf8e06f875d
komed3 <webmaster@komed3.de>
2026-09-03 14:24:15 +0200

    update External.tsx

--- src/components/External.tsx
+++ src/components/External.tsx
@@ -5,16 +5,21 @@ import { SiGithub, SiLinux, SiNpm, SiPython } from 'react-icons/si';
 const LINKS = [
   { url: 'https://github.com/komed3', label: 'GitHub', icon: SiGithub },
   { url: 'https://npmjs.com/~komed3', label: 'npm', icon: SiNpm },
-  { url: 'https://deb.komed3.de', label: 'APT Repo', icon: SiLinux },
+  { url: 'https://deb.komed3.de', label: 'Linux', icon: SiLinux },
   { url: 'https://pypi.org/user/komed3', label: 'Pypi', icon: SiPython }
 ] as const;
 
 
 export function External () {
   return (
-    <div className= 'flex gap-12 px-12 py-16'>
+    <div className= 'grid md:grid-cols-2 2xl:flex gap-6 sm:gap-12 px-6 sm:px-12 py-16 whitespace-nowrap'>
       { /** Label */ }
-      <div className= 'shrink-0 flex flex-col text-2xl uppercase font-extralight tracking-widest'>
+      <div
+        className= {
+          'shrink-0 md:col-span-2 flex 2xl:flex-col flex-wrap gap-x-2 text-xl sm:text-2xl uppercase ' +
+          'font-extralight tracking-widest'
+        }
+      >
         <span>Visit</span>
         <span>my</span>
         <span>work</span>
