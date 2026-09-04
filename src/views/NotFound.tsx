21ce26de127331985fb51e92354d142f3d46655f
komed3 <webmaster@komed3.de>
2026-09-05 01:03:51 +0200

    prepare motion

--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,6 +1,16 @@
+import { motion } from 'motion/react';
+
+
 export function NotFound () {
   return (
     <div className= 'w-full h-screen'>
+      <motion.div
+        initial= { { y: 40, opacity: 0 } }
+        whileInView= { { y: 0, opacity: 1 } }
+        transition= { { duration: 1.2, ease: [ 0.22, 1, 0.36, 1 ] } }
+        viewport= { { once: true, amount: 0.3 } }
+        className= 'centered w-full px-6 sm:px-12 lg:px-20'
+      ></motion.div>
     </div>
   );
 }
