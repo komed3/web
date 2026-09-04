b2eb01b819484b890c45e7d37dc743b537997410
komed3 <webmaster@komed3.de>
2026-09-04 15:14:50 +0200

    fetch project by its ID

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -1,8 +1,14 @@
-import { useParams } from 'react-router';
+import { useNavigate, useParams } from 'react-router';
+
+import projects from '../../data/projects.json';
 
 
 export function Project () {
+  const navigate = useNavigate();
+
   const { id } = useParams();
+  const index = projects.findIndex( p => p.id === id );
+  const project = projects[ index ];
 
   return ( <></> );
 }
